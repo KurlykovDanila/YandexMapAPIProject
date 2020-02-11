54640ce1a75118ae06875e6f4f1a048b3f42c637
Qolorerr <aue@aue.cum>
2020-02-11 18:03:03 +0700

    Working first task

--- main.py
+++ main.py
@@ -1,52 +1,77 @@
 import os
 import sys
-
 import requests
+from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt5.QtCore import Qt
 
-SCREEN_SIZE = [600, 450]
 
-
-class Example(QWidget):
+class Map(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.getImage()
-        self.initUI()
+        self.coordinates = ['82.920430', '55.030199']
+        self.scale = 10
+        self.init_ui()
 
-    def getImage(self):
-        map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
-        response = requests.get(map_request)
+    def get_image(self):
+        map_api_server = "http://static-maps.yandex.ru/1.x/"
+        map_params = {
+            "ll": ",".join(self.coordinates),
+            "z": str(self.scale),
+            "l": "map"
+        }
+        response = requests.get(map_api_server, params=map_params)
 
         if not response:
             print("Ошибка выполнения запроса:")
-            print(map_request)
+            print(response.url)
             print("Http статус:", response.status_code, "(", response.reason, ")")
-            sys.exit(1)
+            return
 
-        # Запишем полученное изображение в файл.
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
 
-    def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
-
-        ## Изображение
+    def change_map(self):
+        self.get_image()
         self.pixmap = QPixmap(self.map_file)
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
-        self.image.setPixmap(self.pixmap)
+        self.mapImg.setPixmap(self.pixmap)
 
-    def closeEvent(self, event):
-        """При закрытии формы подчищаем за собой"""
+    def init_ui(self):
+        uic.loadUi('window.ui', self)
+        self.change_map()
+        self.pbShow.clicked.connect(self.get_coordinates)
+
+    def get_coordinates(self):
+        x = self.lineGetX.text().strip()
+        try:
+            float(x)
+        except ValueError:
+            return
+        y = self.lineGetY.text().strip()
+        try:
+            float(y)
+        except ValueError:
+            return
+        self.coordinates = [x, y]
+        self.change_map()
+
+    def keyPressEvent(self, button):
+        if button.key() == Qt.Key_PageUp:
+            self.scale = min(self.scale + 1, 20)
+        elif button.key() == Qt.Key_PageDown:
+            self.scale = max(self.scale - 1, 0)
+        else:
+            return
+        self.change_map()
+
+    def close_event(self, event):
         os.remove(self.map_file)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = Map()
     ex.show()
     sys.exit(app.exec())
