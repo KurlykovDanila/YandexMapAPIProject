b0d1e7ecaa7c15d2721a765a2c865a86505fe380
KurlykovDanila <56872251+kurlykovdanila@users.noreply.github.com>
2020-02-13 21:31:09 +0700

    add move with help arrow keys, but i need ask teacher about it

--- main.py
+++ main.py
@@ -16,13 +16,15 @@ class Map(QMainWindow):
         self.coordinates = ['82.920430', '55.030199']
         self.scale = 10
         self.init_ui()
+        self.step = 360 / (2 ** (self.scale))
 
     def get_image(self):
         map_api_server = "http://static-maps.yandex.ru/1.x/"
         map_params = {
             "ll": ",".join(self.coordinates),
             "z": str(self.scale),
-            "l": "map"
+            "l": "map",
+            "size": "450,450"
         }
         response = requests.get(map_api_server, params=map_params)
 
@@ -69,9 +71,18 @@ class Map(QMainWindow):
             self.scale = min(self.scale + 1, 20)
         elif button.key() == Qt.Key_PageDown:
             self.scale = max(self.scale - 1, 0)
+        elif button.key() == Qt.Key_Right:
+            self.coordinates[0] = str(float(self.coordinates[0]) + self.step)
+        elif button.key() == Qt.Key_Left:
+            self.coordinates[0] = str(float(self.coordinates[0]) - self.step)
+        elif button.key() == Qt.Key_Up:
+            self.coordinates[1] = str(float(self.coordinates[1]) + self.step)
+        elif button.key() == Qt.Key_Down:
+            self.coordinates[1] = str(float(self.coordinates[1]) - self.step)
         else:
             return
         self.sbScale.setValue(self.scale)
+        self.step = 360 / (2 ** (self.scale))
         self.change_map()
 
     def closeEvent(self, event):
